f4f95372db10daa04558d6b949f2c004ef04e575
Andrew Sotnikov <andrew.sotnikov@zoho.com>
2021-04-04 12:46:07 +0400

    Initial view of installer. Added UI.

---
+++ Installer/Build.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Installer
+{
+    public class Build
+    {
+        public string VersionName { get; set; }
+        public string Url { get; set; }
+        public Build(string versionName, string url)
+        {
+            VersionName = versionName;
+            Url = url;
+        }
+
+
+    }
+}
---
+++ Installer/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Builds.Add(new Build("v0.1.0", "url1"));
+            Builds.Add(new Build("v1.1", "url2"));
+
+            InitializeComponent();
+            DataContext = this;
+
+
+        }
+
+        public ObservableCollection<Build> Builds { get; set; } = new ObservableCollection<Build>();
+    }
+}
